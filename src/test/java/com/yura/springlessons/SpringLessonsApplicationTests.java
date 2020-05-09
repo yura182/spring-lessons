4e82172a2465bd0375429810bf53152c2ce3fce0
Yura <yuriy.petrashenko7@gmail.com>
2020-05-09 19:36:40 +0300

    Intital commit

---
+++ src/test/java/com/yura/springlessons/SpringLessonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yura.springlessons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLessonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
